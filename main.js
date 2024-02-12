7100678ea12917813646cb974fc579a5f099d894
ymustefa <yasminmustefa2026@u.northwestern.edu>
2024-02-12 15:22:23 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+document.querySelector("#chanterelle-button").addEventListener("click", function() {
+    document.querySelector("#header").innerHTML = "Chanterelle"
+    document.querySelector("#image").src = "chanterelle.jpg"
+    document.querySelector("#credit".innerHTML = "Neil Leclerc")
+}
+) 
+
+document.querySelector("#oyster-button").addEventListener("click", function() {
+    document.querySelector("#header").innerHTML = "Oyster"
+    document.querySelector("#image").src = "oyster.jpg"
+    document.querySelector("#credit".innerHTML = "Neil Leclerc")
+}
+) 
+
+document.querySelector("#fly-agaric-button").addEventListener("click", function() {
+    document.querySelector("#header").innerHTML = "fly agaric"
+    document.querySelector("#image").src = "fly-agaric.jpg"
+    document.querySelector("#credit".innerHTML = "Neil Leclerc")
+}
+) 
+    
